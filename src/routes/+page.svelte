03681cd92394921344f40bec18b84b83baaf53ac
forrestbalman <ifork1@gmail.com>
2023-10-18 02:18:40 -0700

    Changes to layout and visibility

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import { onMount } from "svelte";
     import { fade } from "svelte/transition";
     import { cubicOut } from "svelte/easing";
-    import { Howl, Howler } from "howler";
+    import { Howl } from "howler";
 
     let containerHeight;
     let panelHeight;
@@ -12,18 +12,29 @@
     let animationRate;
     let base;
     let incidentals;
+    let bgOpacity;
+    let endPiece = false;
 
     $: if (waterLevel) {
         panels = Array.from({ length: 144 }, () => ({ opacity: 0, color: randomGray() }));
+        bgOpacity = linearConversion(waterLevel, 2, 8, 0, 1);
     }
 
     $: if (start) {
         setTimeout(() => {
             base.play();
-            base.fade(0, 1, 1000);
+            base.fade(0, 1, 3000);
         });
 
-        setTimeout(animate, 5000);
+        setTimeout(animate, 3000);
+    }
+
+    $: if (endPiece) {
+        base.fade(1, 0, 10000);
+
+        setTimeout(() => {
+            location.reload();
+        }, 10000);
     }
 
     async function fetchTides() {
@@ -37,19 +48,25 @@
         }
     }
 
+    function linearConversion(oldValue, oldMin, oldMax, newMin, newMax) {
+        const oldRange = oldMax - oldMin;
+        const newRange = newMax - newMin;
+        return ((oldValue - oldMin) * newRange) / oldRange + newMin;
+    }
+
     function randomGray() {
-        // random number between 3 and 7 inclusive
         const num = Math.floor(Math.random() * 5) + 3;
         return `#${num}${num}${num}${num}${num}${num}`;
     }
 
     function randomClick() {
-        const chance = Math.random() * 50;
+        const chance = Math.random() * (100 / waterLevel);
 
-        if (chance < waterLevel) {
+        if (chance < waterLevel && !endPiece) {
             const randomIndex = Math.floor(Math.random() * incidentals.length);
             const click = incidentals[randomIndex];
 
+            click.volume(Math.random() * 0.5 + 0.5);
             click.seek(0);
             click.play();
         }
@@ -63,7 +80,6 @@
 
         panels = [...panels];
 
-        //set animation rate to be between 900 and 1300 ms then multiply by the water level / 5
         animationRate = (Math.floor(Math.random() * 400) + 900) * (waterLevel / 5);
         randomClick();
 
@@ -84,28 +100,36 @@
     });
 </script>
 
-<div class="bg vh-100 overflow-hidden" bind:clientHeight={containerHeight}>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+{#if !start}
     {#if !waterLevel}
         <div class="w-100 position-absolute top-50 start-50 translate-middle d-flex flex-column align-items-center">
             <div class="spinner-border text-light" role="status">
                 <span class="visually-hidden">Loading...</span>
             </div>
         </div>
-    {:else if !start}
-        <div class="w-100 position-absolute top-50 start-50 translate-middle d-flex flex-column align-items-center" transition:fade>
-            <h1 class="display-1 text-center mb-4">And Where Do I Seek The Dolphin Of Wisdom?</h1>
-            <button class="btn btn-lg btn-outline-dark shadow-lg fs-2 fw-medium border border-3 border-dark px-5 py-3" transition:fade={{ easing: cubicOut }} on:click={() => (start = true)}>Start</button>
-        </div>
     {:else}
-        <div class="row" transition:fade={{ easing: cubicOut }}>
-            {#each panels as panel}
+        <div class="w-100 position-absolute top-50 start-50 translate-middle d-flex flex-column align-items-center" transition:fade>
+            <h1 class="display-1 text-center text-light mb-4">And Where Do I Seek The Dolphin Of Wisdom?</h1>
+            <button class="btn btn-lg btn-outline-light shadow-lg fs-2 fw-medium border border-3 border-light px-5 py-3" transition:fade={{ easing: cubicOut }} on:click={() => (start = true)}>Start</button>
+        </div>
+    {/if}
+{:else}
+    <div class="bg w-100 vh-100 position-absolute overflow-hidden" style="opacity: {bgOpacity};" bind:clientHeight={containerHeight} transition:fade={{ duration: 3000, easing: cubicOut }}>
+        <div class="row">
+            {#each panels as panel, i}
                 <div class="col-1 p-0" style="opacity: {panel.opacity}; transition: opacity {animationRate}ms cubic-bezier(0,.45,.9,1);">
-                    <div class="panel w-100" style="height: {containerHeight / 12}px; background: {panel.color}; transition: background {animationRate}ms cubic-bezier(0,.45,.9,1);" />
+                    {#if i === 143}
+                        <div class="panel w-100" style="height: {containerHeight / 12}px; background: {panel.color}; transition: background {animationRate}ms cubic-bezier(0,.45,.9,1); cursor: pointer;" on:click={() => (endPiece = true)} />
+                    {:else}
+                        <div class="panel w-100" style="height: {containerHeight / 12}px; background: {panel.color}; transition: background {animationRate}ms cubic-bezier(0,.45,.9,1);" />
+                    {/if}
                 </div>
             {/each}
         </div>
-    {/if}
-</div>
+    </div>
+{/if}
 
 <style>
     .bg {
